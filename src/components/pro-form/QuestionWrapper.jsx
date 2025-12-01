e12385b4c42da079e8481d099d5ba880f76317af
Benjamin “Isaac” Hines <benjamin.hines.kaseya@gmail.com>
2025-12-01 18:42:02 +0000

    File changes

--- src/components/pro-form/QuestionWrapper.jsx
+++ src/components/pro-form/QuestionWrapper.jsx
@@ -20,7 +20,7 @@ export default function QuestionWrapper({
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div id={id} className="space-y-4">
+    <div id={id} className={`space-y-4 ${isExpanded ? 'my-[2%]' : 'mb-[0.5%]'}`}>
       <div className="flex items-start gap-3">
         <div 
           className={`block flex-1 ${isCollapsible ? 'cursor-pointer' : ''}`}
