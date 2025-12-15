1a394ff66b9f44c090ec26a91c5595f2605afd17
Benjamin “Isaac” Hines <benjamin.hines.kaseya@gmail.com>
2025-12-15 15:31:35 +0000

    File changes

--- src/components/pro-form/MultiGeographicQuestion.jsx
+++ src/components/pro-form/MultiGeographicQuestion.jsx
@@ -39,7 +39,7 @@ export default function MultiGeographicQuestion({
     }
 
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&loading=async`;
     script.async = true;
     script.defer = true;
     script.onload = () => {
