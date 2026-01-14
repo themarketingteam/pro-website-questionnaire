29256f57575d6602c9479e99122f9d8e97c5834f
Benjamin “Isaac” Hines <benjamin.hines.kaseya@gmail.com>
2026-01-14 20:19:38 +0000

    File changes

--- src/components/ReduxProvider.jsx
+++ src/components/ReduxProvider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '../store/store';
+import { store, persistor } from './store/store';
 
 export default function ReduxProvider({ children }) {
   return (
